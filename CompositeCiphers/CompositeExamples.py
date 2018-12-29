db78f60c9c2a11c6a2bf91ae84917f6d071510ba
SymmetricChaos <ajfraebel@gmail.com>
2018-12-29 15:09:21 -0500

    Moving Stuff Around Again

--- CompositeCiphers/CompositeExamples.py
+++ CompositeCiphers/CompositeExamples.py
@@ -10,6 +10,16 @@ def Checkerboard_DRYAD(text,keys,decode=False):
 def Vigenere_Columnar(text,keys,decode=False):
     return compositeCipher(text,[vigenere,columnarTransport],keys,decode=decode)
 
+def Checkerboard_DRYAD_Example():
+    print("Example of the Checkerboard Dryad Cipher\n")
+    key = [["ZEBRA",[1,3]],7656751]
+    print("They key is {}\n".format(key))
+    ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
+    ctext = Checkerboard_DRYAD(ptext,key)
+    dtext = Checkerboard_DRYAD(ctext,key,decode=True)
+    print("Plaintext is:  {}".format(ptext))
+    print("Ciphertext is: {}".format(ctext))
+    print("Decodes As:    {}".format(dtext))
 
 def Vigenere_Columnar_Example():
     print("Example of the Vigenere Columnar Cipher\n")
@@ -23,18 +33,6 @@ def Vigenere_Columnar_Example():
     print("Decodes As:    {}".format(dtext))
     
 
-def Checkerboard_DRYAD_Example():
-    print("Example of the Checkerboard Dryad Cipher\n")
-    key = [["ZEBRA",[1,3]],7656751]
-    print("They key is {}\n".format(key))
-    ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
-    ctext = Checkerboard_DRYAD(ptext,key)
-    dtext = Checkerboard_DRYAD(ctext,key,decode=True)
-    print("Plaintext is:  {}".format(ptext))
-    print("Ciphertext is: {}".format(ctext))
-    print("Decodes As:    {}".format(dtext))
-    
-    
 Checkerboard_DRYAD_Example()
 print("\n\n")
 Vigenere_Columnar_Example()
---
+++ CompositeCiphers/justatest.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto")
+from Codes import BaconCipher
+
