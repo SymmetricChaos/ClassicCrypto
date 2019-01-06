39b455855e407dde22230fae15b7c2ca06a5757a
SymmetricChaos <ajfraebel@gmail.com>
2019-01-06 17:12:52 -0500

    Improved Hill Climbing Output
    Output looks nicer and has a progress bar between keys.

--- Attacks/HillClimbingPlayfair.py
+++ Attacks/HillClimbingPlayfair.py
@@ -1,8 +1,8 @@
-# based on http://practicalcryptography.com/cryptanalysis/stochastic-searching/cryptanalysis-simple-substitution-cipher/
+# Testing if the simple Hill Climbing method will work on the Playfair cipher
 
 import sys
 sys.path.append("C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto")
-from Ciphers import Monoalphabetic as sub
+from Ciphers import Playfair as pf
 from TextScoring import quadgramScore
 import random
 
@@ -22,18 +22,18 @@ def hillclimbing(ctext):
     # space.
     for x in range(1000):
         # To start the round we randomize the alphabet to start with
-        key = [i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+        key = [i for i in "ABCDEFGHIKLMNOPQRSTUVWXYZ"]
         random.shuffle(key)
         
         # Our starting score is whatever we got from this
-        out = sub.substitution(ctext,"".join(key),decode=True)
+        out = pf.playfairCipher(ctext,"".join(key),decode=True)
         bestscore = quadgramScore(out)
         bestkey = "".join(key)
      
         # Within each round we keep mutating the key until we go a few thousand
         # mutations without improvement.
         ctr = 0
-        while ctr < 2000:
+        while ctr < 1000:
             # Count how many mutations since the last improvement
             ctr += 1
             
@@ -41,12 +41,12 @@ def hillclimbing(ctext):
             newKey = key[:]
             
             # The mutation is swapping two letters
-            A = random.randint(0,25)
-            B = random.randint(0,25)
+            A = random.randint(0,24)
+            B = random.randint(0,24)
             newKey[A],newKey[B] = newKey[B],newKey[A]
             
             # Try it and see what score we get
-            out = sub.substitution(ctext,"".join(newKey),decode=True)
+            out = pf.playfairCipher(ctext,"".join(newKey),decode=True)
             score = quadgramScore(out)
             
             # If that score is better than before write it down and reset the
@@ -66,17 +66,15 @@ def hillclimbing(ctext):
             finalScore = bestscore
             print("\n\nRound {}".format(x))
             print("Key Looks Like:")
-            print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            #print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
             print("".join(finalKey))
             print()
-            print(sub.substitution(ctext,"".join(finalKey),decode=True))
+            print(pf.playfairCipher(ctext,"".join(finalKey),decode=True))
             print("\n")
         else:
             print("#",end="")
-            if (x + 1) % 40 == 0:
+            if (x + 1) % 50 == 0:
                 print()
-
-
 
 ctext = "SOWFBRKAWFCZFSBSCSBQITBKOWLBFXTBKOWLSOXSOXFZWWIBICFWUQLRXINOCIJLWJFQUNWXLFBSZXFBTXAANTQIFBFSFQUFCZFSBSCSBIMWHWLNKAXBISWGSTOXLXTSWLUQLXJBUUWLWISTBKOWLSWGSTOXLXTSWLBSJBUUWLFULQRTXWFXLTBKOWLBISOXSSOWTBKOWLXAKOXZWSBFIQSFBRKANSOWXAKOXZWSFOBUSWJBSBFTQRKAWSWANECRZAWJ"
 
