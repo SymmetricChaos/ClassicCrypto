e22d03f9415b2bc469e4cad3ddb30b731e260ed7
SymmetricChaos <ajfraebel@gmail.com>
2019-01-12 18:40:56 -0500

    Better Autokey Cracker
    Okay I did that thing I would do ten minutes ago.

--- Attacks/AutokeyCracker.py
+++ Attacks/AutokeyCracker.py
@@ -1,30 +1,38 @@
 import sys
 sys.path.append("C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto")
 from Ciphers import VigenereCipher as vig
-from TextScoring import quadgramScore, bigramScore
+from TextScoring import bigramScore
 
 
 
-def autokeyAttack(ctext,klen):
-    k = ["A"]*klen
-    bestKey = k[:]
-    bestScore = quadgramScore(ctext)
-
-    for i in range(klen):
-        for l in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            tempKey = bestKey[:]
-            tempKey[i] = l
-            dtext = vig.vigenereAutokey(ctext,tempKey,decode=True)
-            score = bigramScore(dtext)
-            if score > bestScore:
-                bestKey = tempKey
-                bestScore = score
-    print(bestKey)
-    print(vig.vigenereAutokey(ctext,bestKey,decode=True))
+def autokeyAttack(ctext,limit=30):
+    outKey = ["A"]
+    outScore = bigramScore(ctext)
+    for klen in range(2,limit):
+        bestKey = ["A"]*klen
+        bestScore = bigramScore(ctext)
     
+        for i in range(klen):
+            for l in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                tempKey = bestKey[:]
+                tempKey[i] = l
+                dtext = vig.vigenereAutokey(ctext,tempKey,decode=True)
+                score = bigramScore(dtext)
+                if score > bestScore:
+                    bestKey = tempKey
+                    bestScore = score
+        if bestScore > outScore:
+            outScore = bestScore
+            outKey = bestKey
+    print("Best Key Found:")
+    print("".join(outKey))
+    print()
+    print(vig.vigenereAutokey(ctext,outKey,decode=True))
+    print()
+        
     
     
 ptext = "THECULTIVATIONOFTHESUGARCANEISPURSUEDTOGREATEXTENTINTHEISLANDSOFTHEWESTINDIESWHEREABOUTTHREECENTURIESAGOITWASFIRSTINTRODUCEDFROMCHINAORSOMEOTHERPARTSOFTHEEASTANDWHEREITFLOURISHESWITHGREATLUXURIANCEPARTICULARLYINMOISTANDRICHGROUNDTHESEASONFORPLANTINGITCOMMENCESABOUTTHEBEGINNINGOFAUGUST"
-ctext = vig.vigenereAutokey(ptext,"MYKEYVALUE")
+ctext = vig.vigenereAutokey(ptext,"ALONGBITOFGIBBERISH")
 
-autokeyAttack(ctext,10)
+autokeyAttack(ctext)
