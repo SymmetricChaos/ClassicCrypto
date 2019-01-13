f1b614f6f775901334bc14822662167c8b29fc75
SymmetricChaos <ajfraebel@gmail.com>
2019-01-13 17:34:03 -0500

    Bacon Cipher and Bacon Code
    Divided the Bacon Cipher from the Bacon Code. Now any binary code can be used to generate the text of a Bacon Cipher.

--- Codes/BinaryCodes.py
+++ Codes/BinaryCodes.py
@@ -1,7 +1,8 @@
 
 
-# Morse code is a very popular method of encoding letters and numbers
-# variations exist for languages other than English.
+# Morse code is a very popular method of encoding letters and numbers. It is
+# not technically a binary code since it requires dots, dashes, and spaces in
+# order to be interpreted.
 
 import random
 from UtilityFunctions import groups
@@ -44,65 +45,7 @@ def morseCode(text,decode=False):
         
         return "".join(out)
 
-    
 
-
-# The so-called Bacon Cipher is actually a simple code that is well suited to 
-# being hidden. This implementation hides the code as uppercase and lower case 
-# letters.
-
-
-def baconCipher(text,stegotext="",decode=False):
-    
-    # If using the stegotext option there has to be enough text
-    if stegotext != "":
-        if len(stegotext) < len(text)*5:
-            raise Exception('not enough stegotext provided')
-        
-    UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    LOWER = "abcdefghijklmnopqrstuvwxyz"
-    codes = ["00000","00001","00010","00011","00100","00101","00110","00111",
-             "01000","01001","01010","01011","01100","01101","01110","01111",
-             "10000","10001","10010","10011","10100","10101","10110","10111",
-             "11000","11001"]
-    
-    if decode == False:
-        out = []
-        # If not using any stegotext random letters are picked
-        if stegotext == "":
-            for letter in text:
-                code = codes[UPPER.index(letter)]
-                
-                for bit in code:
-                    if bit == "0":
-                        out.append(random.choice(LOWER))
-                    if bit == "1":
-                        out.append(random.choice(UPPER))
-        else:
-            ctr = 0
-            for letter in text:
-                code = codes[UPPER.index(letter)]
-                
-                for bit in code:
-                    if bit == "0":
-                        out.append(stegotext[ctr].lower())
-                    if bit == "1":
-                        out.append(stegotext[ctr].upper()) 
-                    ctr += 1
-
-        return "".join(out)
-    
-    if decode == True:
-        out = ""
-        for letters in groups(text,5):
-            code = ""
-            for bit in letters:
-                if bit.islower():
-                    code += "0"
-                if bit.isupper():
-                    code += "1"
-            out += UPPER[codes.index(code)]
-        return out
 
 # A prefix code is any code in which words can be of various lengths but which
 # does not need commas to show the breaks between them. There are many ways to
@@ -186,6 +129,78 @@ def prefixCode(text,decode=False):
                 out.append(D[code])
                 code = ""
         return "".join(out)
+    
+# Usually called the "Bacon Cipher" this is really a simple binary code. Every
+# letter is represented by a five bit code. This would allow for thirty two
+# characters, though only 26 are used. The next section adapted Bacon's method
+# of stegonography to any binary code.
+def baconCode(text,decode=False):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    codes = ["00000","00001","00010","00011","00100","00101","00110","00111",
+             "01000","01001","01010","01011","01100","01101","01110","01111",
+             "10000","10001","10010","10011","10100","10101","10110","10111",
+             "11000","11001"]
+    
+    if decode == False:
+        out = ""
+        for letter in text:
+            out += codes[letters.index(letter)]
+
+        return out
+    
+    if decode == True:
+        out = ""
+        for code in groups(text,5):
+            out += letters[codes.index(code)]
+        return out
+    
+# The so-called Bacon Cipher is actually a simple code that is well suited to 
+# being hidden. This implementation hides the code as uppercase and lower case 
+# letters.
+
+
+def baconCipher(text,code,stegotext="",decode=False):
+    
+    UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LOWER = "abcdefghijklmnopqrstuvwxyz"
+
+    
+    if decode == False:
+        ctext = code(text)
+    
+        # If using the stegotext option there has to be enough text
+        if stegotext != "":
+            if len(stegotext) < len(ctext):
+                raise Exception('not enough stegotext provided')
+
+        out = ""
+        # If not using any stegotext random letters are picked
+        if stegotext == "":
+            for bit in ctext:
+                if bit == "0":
+                    out += random.choice(LOWER)
+                if bit == "1":
+                    out += random.choice(UPPER)
+        else:
+            for L1,L2 in zip(ctext,stegotext):
+                if L1 == "0":
+                    out += L2.lower()
+                if L1 == "1":
+                    out += L2.upper()
+
+        return out
+    
+    if decode == True:
+        dtext = ""
+        for letter in text:
+            if letter.islower():
+                dtext += "0"
+            if letter.isupper():
+                dtext += "1"
+        
+        out = code(dtext,decode=True)
+            
+        return out
        
 def morseCodeExample():
     print("Example of Morse Code\n")
@@ -201,15 +216,32 @@ def prefixCodeExample():
     print(ptext)
     print(ctext)
     
-def baconCipherExample():
-    print("Example of the Bacon Cipher\n")
+def baconCodeExample():
+    print("Example of the Bacon Code\n")
     ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
-    ctext = baconCipher(ptext)
+    ctext = baconCode(ptext)
     print(ptext)
     print(ctext)
+    
+def baconCipherExample():
+    print("Example of the Bacon Cipher\n\n")
+    ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
+    for codeType in [prefixCode,baconCode]:
+        print("Using {}:\n".format(codeType.__name__))
+        ctext = baconCipher(ptext,codeType)
+        dtext = baconCipher(ctext,codeType,decode=True)
+        print(ptext)
+        print(ctext)
+        if ptext == dtext:
+            print("\n(Decode Works)")
+        else:
+            print("\n(Decode Failed)")
+        print("\n")
 
 #morseCodeExample()
 #print()
 #prefixCodeExample()
 #print()
-#baconCipherExample()
+#baconCodeExample()
+#print()
+baconCipherExample()
