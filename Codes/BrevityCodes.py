ffec73db36cbca27d83aa35f8f095cfb95521a08
SymmetricChaos <ajfraebel@gmail.com>
2019-01-14 08:15:55 -0500

    Update BrevityCodes.py
    We use an asterisk to prevent changing the end of a word more than once. Also it is used prevent changing words that are equivalent to suffices.

--- Codes/BrevityCodes.py
+++ Codes/BrevityCodes.py
@@ -6,8 +6,12 @@ from PrepareText import preptext3
 textfile = open('C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto\\SampleText\\WindyHill.txt', 'r')
 ptext = preptext3(textfile.readline())
 
+
 def suffixes(text):
-    codes = ["BY","C","CL","CY","D","E","F","FY","G"]
+    codes = ["BY","C","CL","CY","D","E","F","FY","G","GY",
+             "H","HD","J","L","LY","LS","M","N","NS","R",
+             "RY","S","SM","SP","SR","ST","T","TS","TY",
+             "TZ","U","V","VY","WD","X","Y","Z"]
     suffs = [["ABILITY","IBILITY","ABLY","IBLY"],
              ["ANCE","ENCE","ATIC","IC"],
              ["ICAL"],
@@ -16,10 +20,26 @@ def suffixes(text):
              ["EE"],
              ["FUL","SELF"],
              ["FULLY","IFY"],
-             ["ING"]]
+             ["ING"],
+             ["OLOGY"],
+             ["ISH","TH"],
+             ["HOOD"],
+             ["AGE"],
+             ["ABLE","IBLE","BLE","IAL","UAL","AL"],
+             ["ALLY","ILITY","ILY","LTY"],
+             ["LESS"],
+             ["ISM","MENT"],
+             ["ATION","ITION","TION","SION","IAN"]]
+    
+    for j in suffs:
+        for x in j:
+            text = text.replace(" {} ".format(x)," {}* ".format(x))
+    
     for i,j in zip(codes,suffs):
         for x in j:
-            text = text.replace(x + " ", i + " ")
+            text = text.replace(x + " ", i + "* ")
+
+    text = text.replace("*", "")
 
     return text
     
