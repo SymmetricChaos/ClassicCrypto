a2f8bfaef579377d0f7996b11b26c726e6b68eb4
SymmetricChaos <ajfraebel@gmail.com>
2019-01-23 23:21:14 -0500

    Two Square Working
    Two square cipher now works

--- Ciphers/TwoSquare.py
+++ Ciphers/TwoSquare.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto")
 from Ciphers.UtilityFunctions import groups, alphabetPermutation
 import numpy as np
 
@@ -8,18 +10,16 @@ def twoSquare(text,keys,decode=False,printkey=False):
 
     sq1 = np.full([6,6],"")
     sq2 = np.full([6,6],"")
-    alphasq = np.full([6,6],"")
     for i in range(6):
         sq1[i] = [x for x in groups(k1,6)[i]]
         sq2[i] = [x for x in groups(k2,6)[i]]
-        alphasq[i] = [x for x in groups("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",6)[i]]
 
     if printkey == True:
         for i in range(6):
             print(" ".join(sq1[i]))
         print()
         for i in range(6):
-            print(" ".join(sq2[i]),end="  ")
+            print(" ".join(sq2[i]))
         return ""
 
 
@@ -28,25 +28,25 @@ def twoSquare(text,keys,decode=False,printkey=False):
     G = groups(text,2)
     
     
-    if decode == False:
-        out = ""
-        for g in G:
-            # MAKE IT DO THE STUFF
-            pass
-
-    
-    if decode == True:
-        out = ""
-        for g in G:
-            # MAKE IT DO THE OTHER STUFF
-            pass
+    # The two square cipher is involutive which means that encryption is
+    # exactly the same as decryption. The decode argument is ignored but must
+    # exist for compatibility.
+    out = ""
+    for g in G:
+        A = np.where(sq1 == g[0])
+        B = np.where(sq2 == g[1])
+                
+        out += sq1[A[0],B[1]][0]
+        out += sq2[B[0],A[1]][0]
+            
+    return out
 
     
 def twoSquareExample():
-    print("Example of the Four Square Cipher\n")
+    print("Example of the Two Square Cipher\n")
     print("The key is:")
     keys = ["953SQUARE178","621CIPHER457"]
-    fourSquare("TEST",keys,printkey=True)
+    twoSquare("",keys,printkey=True)
     
     print("")
     ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
@@ -55,3 +55,5 @@ def twoSquareExample():
     print("Plaintext is:  {}".format(ptext))
     print("Ciphertext is: {}".format(ctext))
     print("Decodes As:    {}".format(dtext))
+    
+#twoSquareExample()
