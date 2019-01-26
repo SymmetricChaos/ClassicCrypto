88f2994c7871f3b56232f2a42637db4b86e15d68
SymmetricChaos <ajfraebel@gmail.com>
2019-01-25 22:45:05 -0500

    Improved Hill Cipher Attack

--- Attacks/HillCipherCracker.py
+++ Attacks/HillCipherCracker.py
@@ -3,15 +3,18 @@ sys.path.append("C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto")
 from Ciphers.HillCipher import hillCipher
 from Ciphers.UtilityFunctions import groups
 from sympy import Matrix, pprint
-
+from Ciphers.PrepareText import preptext1
+from Attacks.TextScoring import quadgramScore
 
 def hillCipherAttack(ctext,crib):
+    
+    bestScore = quadgramScore(ctext)
+    bestKey = Matrix([[1,1],[1,1]])
     
     # We can use this to reduce a matrix modulo 26
     mod26 = lambda x : x % 26
     
-
-    
+    # Convert the text and crib to numbers so they're more useful
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     ctextN = [alpha.index(i) for i in ctext]
     cribN = [alpha.index(i) for i in crib]
@@ -19,31 +22,47 @@ def hillCipherAttack(ctext,crib):
     # Break text into pieces
     G = groups(ctextN,2)
     
-    print(G)
-    
-    A = Matrix([cribN[:2],cribN[2:]]).T
-    
-    for i in range(len(G)-1):
-        B = Matrix([G[i],G[i+1]]).T
+    for pos in range(len(crib)-3):
         
-        if B.det() % 2 == 0:
-            continue
+        subCrib = cribN[pos:pos+4]
         
-        if B.det() % 13 == 0:
-            continue
-    
-        C = A*(B.inv_mod(26))
+        #print(subCrib)
         
-        tKey = C.applyfunc(mod26)
+        A = Matrix([subCrib[:2],subCrib[2:]]).T
+        
+        for i in range(len(G)-1):
+            B = Matrix([G[i],G[i+1]]).T
+            
+            # If the matrix is not invertible
+            if B.det() % 2 == 0 or B.det() % 13 == 0:
+                continue
+            
+            C = A*(B.inv_mod(26))
+            
+            tKey = C.applyfunc(mod26)
+        
+            # We run the Hill Cipher in encryption mode NOT decryption
+            t = hillCipher(ctext,tKey)
+            score = quadgramScore(t)
+            
+            if score > bestScore:
+                bestScore = score
+                bestKey = tKey
+                #print(t[:40])
+                
+                
     
-        # We run the Hill Cipher in encryption mode NOT decryption
-        print(hillCipher(ctext,tKey))
-
-#ptext = "THECULTIVATIONOFTHESUGARCANEISPURSUEDTOGREATEXTENTINTHEISLANDSOFTHEWESTINDIESWHEREABOUTTHREECENTURIESAGOITWASFIRSTINTRODUCEDFROMCHINAORSOMEOTHERPARTSOFTHEEASTANDWHEREITFLOURISHESWITHGREATLUXURIANCEPARTICULARLYINMOISTANDRICHGROUNDTHESEASONFORPLANTINGITCOMMENCESABOUTTHEBEGINNINGOFAUGUST"
-#ctext = hillCipher(ptext,[[23,20],[1,21]])
-
-ctext = "FUPCMTGZKYUKBQFJHUKTZKKIXTTA"
-crib = "FTHE"
+    return hillCipher(ctext,bestKey)
 
 
-hillCipherAttack(ctext,crib)
+# Load up the text to use
+textfile = open('C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto\\SampleText\\text1.txt', 'r')
+ptext = preptext1(textfile.readline(),silent=True)
+ptext = ptext[:100]
+ctext = hillCipher(ptext,[[23,20],[1,21]])
+
+
+crib = "GREAT"
+
+dtext = hillCipherAttack(ctext,crib)
+print(dtext[:50])
