b97a0236ffb4475e62609402a65ac2eb598576a6
SymmetricChaos <ajfraebel@gmail.com>
2019-01-27 09:59:08 -0500

    Update HillCracker.py
    Better explanation in example

--- Attacks/HillCracker.py
+++ Attacks/HillCracker.py
@@ -30,7 +30,7 @@ def hillCipherAttack(ctext,crib,N):
     for pos in range(len(crib)-(N*N-1)):
         
         subCrib = cribN[pos:pos+(N*N)]
-        
+        #print(subCrib)
         A = Matrix(groups(subCrib,N)).T
         
         
@@ -62,7 +62,6 @@ def hillCipherAttack(ctext,crib,N):
             if score > bestScore:
                 bestScore = score
                 bestKey = tKey
-                #print(t[:40])
                 
     if bestKey.det() % 2 == 0 or bestKey.det() % 13 == 0:
         print("Something Strange Happened")
@@ -87,20 +86,36 @@ known as a "crib", is needed. Here we will try to break the 3x3 version of the
 cipher using a few different cribs.
 In order to make this work we have to try the crib in many positions and do a
 relatively large amount of calculation. As a result we will try our attack on
-just the first 300 characters.
+just the first 600 characters. If that fails we can try with the next section.
+The minimum crib length is 9 but that will only work if the crib perfectly
+lines up with a block. To ensure that we can decrypt the crib must have a
+length of 17. A crib that long is not easy to get.
+Suppose that we known this text is about sugar cane and we know the author has
+the particular oddity of calling mollases "saccharine juice" from other things
+we have seen written. We will try the following likely phrases:
+THEWESTINDIES
+THEISLANDS
+THEMOLASSES
+THESUGARCANE
+SACCHARINEJUICE
 """)
     
     # Load up the text to use
     textfile = open('C:\\Users\\Alexander\\Documents\\GitHub\\ClassicCrypto\\SampleText\\text1.txt', 'r')
     ptext = preptext1(textfile.readline(),silent=True)
-    ptext = ptext[:300]
-    #print(ptext,"\n\n")
+    ptext = ptext[:600]
     key = createMatrixKey(3)
     ctext = hillCipher(ptext,key)
+    print("Cipher Text:")
+    print(ctext,"\n\n")
     
     
-    crib = "OTHERPART"
+    cribs = ["THEWESTINDIES","THEISLANDS","THEMOLASSES","THESUGARCANE","SACCHARINEJUICE"]
     
-    hillCipherAttack(ctext,crib,3)
+    for crib in cribs:
+        print("Using the crib: {}\n".format(crib))
+
+        hillCipherAttack(ctext,crib,3)
+        print("\n\n")
     
 hillCipherAttackExample()
