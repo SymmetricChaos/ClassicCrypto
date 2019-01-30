90081942a0f101977464477261a6b2e360e399ee
SymmetricChaos <ajfraebel@gmail.com>
2019-01-29 19:36:59 -0500

    Caesar Cracker Update
    Caesar cracker is now implemented as a function and has an example.

--- Attacks/CaesarCracker.py
+++ Attacks/CaesarCracker.py
@@ -1,21 +1,43 @@
 from Ciphers.Caesar import caesar
 from TextScoring import quadgramScore
 
-ptext = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
-ctext = caesar(ptext,15)
+# Breaking the Caesar cipher is trivial since there are only 26 possible keys
+# that we have to check. In order to do this automatically we will score the
+# text in terms of how its quadgrams look.
 
-bestkey = 0
-bestdecode = ""
-bestscore = float("-inf")
-for i in range(0,26):
+def caesarCracker(ctext):
+
+    bestkey = 0
+    bestdecode = ""
+    bestscore = float("-inf")
+    for i in range(0,26):
+        
+        dtext = caesar(ctext,i,decode=True)
+        s = quadgramScore(dtext)
+        if s > bestscore:
+            bestkey = i
+            bestscore = s
+            bestdecode = dtext
+
+    print("Best Key Found: {}".format(bestkey))
+    print("Decodes As")
+    print(bestdecode)
+
+def caesarCrackerExample():
     
-    dtext = caesar(ctext,i,decode=True)
-    s = quadgramScore(dtext)
-    if s > bestscore:
-        bestkey = i
-        bestscore = s
-        bestdecode = dtext
+    print("""
+Example of an Attack on the Caesar Cipher
 
-print("Best Key Found: {}".format(bestkey))
-print("Decodes As")
-print(bestdecode)
+Since there are only 26 keys we will check each of them in turn and get the one
+that decodes to look the most like English text. It is finding English text 
+that is the only tricky part.
+""")
+    
+    ptext = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
+    ctext = caesar(ptext,15)
+    
+    print("\nThe ciphertext is:",ctext,"\n")
+    
+    caesarCracker(ctext)
+    
+#caesarCrackerExample()
