99a9478b6e5182487cc54471572a81ca5c102a18
SymmetricChaos <ajfraebel@gmail.com>
2019-02-05 19:02:48 -0500

    Better GUI

--- GUI/caesarGUI.py
+++ GUI/caesarGUI.py
@@ -2,12 +2,12 @@ import tkinter as tk
 from Ciphers.Caesar import caesar
 
 root = tk.Tk()
-root.maxsize(800,800)
-root.minsize(800,800)
+root.maxsize(18001,800)
+root.minsize(1800,1800)
 root.title("Caesar Cipher")
-ptext = tk.Text(root)
-ctext = tk.Text(root)
-key = tk.Text(root)
+ptext = tk.Text(root,height=2,width=20)
+ctext = tk.Text(root,height=2,width=20)
+key = tk.Text(root,height=1,width=20)
 encryptbutton = tk.Button(root, text="Encrypt", command = lambda: caesarText(ptext,ctext,key))
 
 def caesarText(textIn,textOut,key):
@@ -17,7 +17,7 @@ def caesarText(textIn,textOut,key):
     textOut.insert("insert",caesar(theInput,k,decode=False))
 
 ptext.pack()
-encryptbutton.pack()
 key.pack()
+encryptbutton.pack()
 ctext.pack()
 root.mainloop()
