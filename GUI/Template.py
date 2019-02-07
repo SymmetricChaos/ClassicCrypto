924645bfbb26b045f2c0d8282e67e231a7f7c2e8
SymmetricChaos <ajfraebel@gmail.com>
2019-02-07 18:24:13 -0500

    Improvement to Template
    Reset button and Correct Textbox Behavior

--- GUI/Template.py
+++ GUI/Template.py
@@ -2,9 +2,14 @@ import tkinter as tk
 
 def cipherGUI(cipher):
     root = tk.Tk()
+    
     root.maxsize(800,800)
     root.minsize(800,800)
+    
+    # Title of the window
     root.title("{}".format(cipher.__name__))
+    
+    # Three textboxes
     ptext = tk.Text(root,height=4,width=40)
     ctext = tk.Text(root,height=4,width=40)
     key = tk.Text(root,height=1,width=20)
@@ -15,9 +20,9 @@ def cipherGUI(cipher):
     
     # Reset Button
     def Reset(): 
-        ctext.set("")
-        ptext.set("") 
-        key.set("") 
+        ctext.delete("1.0","end")
+        ptext.delete("1.0","end") 
+        key.delete("1.0","end") 
       
     
     # Encrypt function
@@ -28,6 +33,8 @@ def cipherGUI(cipher):
         # Get the key from the key box
         k = key.get("1.0","end")[:-1]
       
+        # Blank the ctext box then put the text in it
+        ctext.delete("1.0","end")
         ctext.insert("insert",cipher(T,k,decode=True)) 
     
     # Decrypt function 
@@ -38,6 +45,8 @@ def cipherGUI(cipher):
         # Get the key from the key box
         k = key.get("1.0","end")[:-1]
         
+        # Blank the ctext box then put the text in it
+        ctext.delete("1.0","end")
         ctext.insert("insert",cipher(T,k,decode=False)) 
       
     # Button to run cipher in encrypt mode
@@ -47,6 +56,10 @@ def cipherGUI(cipher):
     # Button to run cipher in decrypt mode
     decryptbutton = tk.Button(root, text="Decrypt", command = dec,
                               bg = 'lightgreen', font = ('arial',14,'bold'))
+    
+    
+    resetbutton = tk.Button(root, text="Reset", command = Reset, 
+                           bg = 'lightslateblue', font = ('arial',14,'bold'))
     
     
     # Button to run cipher in decrypt mode
@@ -69,6 +82,7 @@ def cipherGUI(cipher):
     
     encryptbutton.place(x=150,y=160)
     decryptbutton.place(x=250,y=160)
+    resetbutton.place(x=400,y=160)
     
     ctext.place(x=150,y=220)
     ctextLab.place(x=40,y=220)
