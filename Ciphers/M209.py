6c0720d30df33b46eaf004c1ed83b004108befb5
SymmetricChaos <ajfraebel@gmail.com>
2019-02-22 08:46:40 -0500

    Create M209.py

---
+++ Ciphers/M209.py
@@ -0,0 +1,10 @@
+# The M209 was among the smallest and simplest of the rotor machines. It was
+# entirely mechanical, requiring no electrical power.
+
+def M209(text,key,decode=False):
+    R1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    R2 = "ABCDEFGHIJKLMNOPQRSTUVXYZ" 
+    R3 = "ABCDEFGHIJKLMNOPQRSTUVX"
+    R4 = "ABCDEFGHIJKLMNOPQRSTU"
+    R5 = "ABCDEFGHIJKLMNOPQRS"
+    R6 = "ABCDEFGHIJKLMNOPQ"
