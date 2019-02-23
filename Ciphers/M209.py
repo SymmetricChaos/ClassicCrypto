065c49f64e711799448c4d86d59f29e1e1a9c073
SymmetricChaos <ajfraebel@gmail.com>
2019-02-23 08:51:22 -0500

    Update M209.py
    Created M209 example to study settings

--- Ciphers/M209.py
+++ Ciphers/M209.py
@@ -14,4 +14,30 @@ def M209(text,key,decode=False):
     R5 = "ABCDEFGHIJKLMNOPQRS"
     R6 = "ABCDEFGHIJKLMNOPQ"
     
+
+
+def M209Example():
     
+    import random
+    
+    # Pins can be in either effective + or ineffective - positions
+    pins = []
+    for p in [26,25,23,21,19,17]:
+         pins.append(random.choices("-+",k=p))
+    
+    for i in pins:
+        print("".join(i))
+    
+    # There are 27 pairs of lugs 
+    # They can be in one of six effective positions or in one of two ineffective
+    # positions. Both lugs can be ineffective but both cannot be assigned to the
+    # save effective position.
+    # REMEMBER PYTHON ARRAYS START AT ZERO BUT M209 SPECIFICATION STARTS AT 1
+    lugs = []
+    for l in range(27):
+        
+        lugs.append(random.sample([0,0,1,2,3,4,5,6],k=2))
+    
+    for i in lugs:
+        print(i)
+M209Example()
