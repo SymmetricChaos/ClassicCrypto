43e20f937c7bf57069478565ca3e6964af11f73f
SymmetricChaos <ajfraebel@gmail.com>
2019-02-23 14:08:50 -0500

    Update M209.py
    Lug and overlap positions
    The overlaps don't work, tho

--- Ciphers/M209.py
+++ Ciphers/M209.py
@@ -14,6 +14,16 @@ def transPins(P):
         out.append( [0 if i == "-" else 1 for i in pins] )
     return out
 
+# Convert the friend [x,y] lug settings into binary lists
+def lugPos(L):
+    lugs = []
+    for l in L:
+        x = [0,0,0,0,0,0]
+        for i in l:
+            if i != 0:
+                x[i-1] = 1
+        lugs.append(x)
+    return lugs
 
 # Count up the lugs in each wheel position keeping in mind that 0 is in effective
 # and that Python arrays start at zero.
@@ -25,28 +35,61 @@ def countLugs(L):
                 count[pos-1] += 1
     return count
 
-def activePins(P):
+#def activePins(P):
+#    out = []
+#    for pinList in P:
+#        s = []
+#        for pos,pin in enumerate(pinList):
+#            if pin == "+":
+#                s.append(pos)
+#        out.append(s)
+#    return out
+
+def ltr2num(K):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     out = []
-    for pinList in P:
-        s = []
-        for pos,pin in enumerate(pinList):
-            if pin == "+":
-                s.append(pos)
-        out.append(s)
+    for i in K:
+        out.append( alpha.index(i) )
     return out
 
-#def overlaps(P,L):
-    
+def num2ltr(K):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    out = []
+    for i in K:
+        out.append( alpha[i] )
+    return out
+
+def overlaps(lugs,lugp):
+    overlaps = {}
+    numOverlap = 0
+    for bar in range(27):
+        ctr = 0
+        for wheel in range(6):
+            if lugp[bar][wheel]:
+               ctr += 1
+        if ctr > 1:
+            numOverlap += 1
+            indx = ""
+            for wheel in range(6):
+                if lugp[bar][wheel]:
+                    indx += str(wheel)
+                if indx in overlaps:
+                    overlaps[indx] += 1 
+                else:
+                    overlaps[indx] = 1 
+    return overlaps
 
 def M209(text,key,decode=False):
     
-    indi = key[0]
+    text = ltr2num(text)
+    indi = ltr2num(key[0])
     pins = transPins(key[1])
     acpins = activePins(key[1])
     lugs = countLugs(key[2])
+    lugp = lugPos(key[2])
+    over = overlaps(lugs,lugp)
     
-    #for setting in [lugs,pins,acpins]:
-    #    for i in setting:
+    #   for i in setting:
     #        print(i)
     #   print()
     
@@ -56,9 +99,11 @@ def M209(text,key,decode=False):
     R4 = "ABCDEFGHIJKLMNOPQRSTU"
     R5 = "ABCDEFGHIJKLMNOPQRS"
     R6 = "ABCDEFGHIJKLMNOPQ"
+    
     wheelLen = [26,25,23,21,19,17]
     sh = [15,14,13,12,11,10]
-    indi = [15,4,14,15,11,4]
+    
+    
     pins[0] = [1,1,0,1,0,0,0,1,1,0,1,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0]
     pins[1] = [1,0,0,1,1,0,1,0,0,1,1,1,0,0,1,0,0,1,1,0,1,0,1,0,0]
     pins[2] = [1,1,0,0,0,0,1,1,0,1,0,1,1,1,0,0,0,1,1,1,1,0,1]
@@ -73,17 +118,36 @@ def M209(text,key,decode=False):
     lugs[4] = 10
     lugs[5] = 3
     
+    lugp = [[0,0,1,0,0,1], [0,1,0,0,0,0], [0,1,0,0,0,0],
+            [0,0,0,0,0,1], [0,1,0,0,0,0], [0,1,0,0,1,0],
+            [1,0,0,0,0,1], [0,1,0,0,0,0], [0,1,0,0,1,0],
+            [1,0,0,0,1,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,1,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            ]
+    
+    over = overlaps(lugs,lugp)
+    print(over)
+    
     L = []
-    for k in range(20):
+    out = []
+    for k,letter in enumerate(text):
         s = 0
         for w in range(6):
             #print(sh[w],indi[w],k,lugs[w])
             s += pins[w][ (sh[w] + indi[w] + k) % wheelLen[w]] * lugs[w]
-        L.append(s)
-        #print(s%26)
+        
+        L.append((letter+s)%26)
+        #print()
         #print()
 
-    printColumns(L,10,4)
+    #printColumns(L,10,4)
+    
+    print(num2ltr(L))
+    
 def M209Example():
     
     import random
@@ -108,6 +172,6 @@ def M209Example():
     
     printColumns(lugs,6)
     
-    M209("THEQUICKBROWNFOX",["AAAAA",pins,lugs])
+    M209("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG",["PEOPLE",pins,lugs])
     
 M209Example()
