8b11f4947ba3a2149945f013dee43af90fab2961
SymmetricChaos <ajfraebel@gmail.com>
2019-02-26 21:26:52 -0500

    Update AffineVigenere.py
    Cleaner implementation

--- Ciphers/AffineVigenere.py
+++ Ciphers/AffineVigenere.py
@@ -1,46 +1,45 @@
-from Ciphers.UtilityFunctions import modinv
-
-def affineVigenere(text,key=[0,1],decode=False):
+from Ciphers.UtilityFunctions import modinv, alphaToNumber, numberToAlpha
+from itertools import cycle
+def affineVigenere(text,key=["A","A"],decode=False):
     
     """
 :param text: The text to be encrypyed. Must be alphanumeric and uppercase. The letter J will be replaced with I.
-:param key: A list of two integers. The first is used for addition. The second for multiplication.
+:param key: A list of two integers. The first is used for mutiplication. The second for addition.
 :param decode: Boolean. If false encrypt plaintext. If true decode ciphertext
     """
     
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#"
-    T = []
-    kLen1 = len(key[0])
-    kLen2 = len(key[1])
-    
-    if "#" in key[1]:
+    # Error if an invalid key value is used
+    if "#" in key[0]:
         raise Exception('cannot use # symbol in multiplicative key')
     
-    # convert the keys to lists of numbers
-    K1, K2, = [],[]
-    for i in key[0]:
-        K1.append(alphabet.find(i))
+    # The 37 symbol alphabet being used
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#"
     
-    for i in key[1]:
-        K2.append(alphabet.find(i))
+    # Convert the plaintext and keys to lists of numbers
+    T = alphaToNumber(text,alphabet)
+    K1, K2, = alphaToNumber(key[0],alphabet),alphaToNumber(key[0],alphabet)
+    
 
-    for ind,let in enumerate(text):
-        N = alphabet.index(let)
+    out = []
+    # Cycle through the two keys as much as needed as we go through the key
+    for keynum1,keynum2,textnum in zip(cycle(K1),cycle(K2),T):
 
+        N = textnum
+        
+        # When encoding multiply and then add
+        # When decoding multiply by the inverse then subtract
         if decode == False:
-            N = (N+K1[ind%kLen1])%37
-            N = (N*(K2[ind%kLen2]+1))%37
+            N = (N*(keynum1 + 1))%37
+            N = (N+keynum2) % 37
         else:
-            inv = modinv(K2[ind%kLen2]+1,37)
+            inv = modinv(keynum1 + 1,37)
+            N = (N-keynum2)%37
             N = (N*inv)%37
-            N = (N-K1[ind%kLen1])%37
         
-        T.append(N)
+        out.append(N)
     
-    for t in range(len(T)):
-        T[t] = alphabet[T[t]]
-        
-    return "".join(T)
+    
+    return "".join(numberToAlpha(out,alphabet))
 
 def affineVigenereExample():
 
@@ -54,3 +53,5 @@ def affineVigenereExample():
     print("Plaintext is:  {}".format(ptext))
     print("Ciphertext is: {}".format(ctext))
     print("Decodes As:    {}".format(dtext))
+    
+#affineVigenereExample()
