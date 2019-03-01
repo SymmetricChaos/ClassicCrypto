1937a7bc3df42efac2e2f901e278bd142519618b
SymmetricChaos <ajfraebel@gmail.com>
2019-02-28 22:28:51 -0500

    Substitution Updated
    Now supports custom alphabet

--- Ciphers/Substitution.py
+++ Ciphers/Substitution.py
@@ -6,19 +6,21 @@ from Ciphers.UtilityFunctions import alphabetPermutation
 # letter of the word, the letter B is turned into the second letter of the
 # word and so on. If the word repeats letters those repetitions are skipped.
 
-def substitution(text,key,decode=False):
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    out = []
+def substitution(text,key,decode=False,alphabet=""):
+    
+    if alphabet == "":
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
     # Derive the internally used key from the input
-    KEY = alphabetPermutation(key)
-            
+    KEY = alphabetPermutation(key,alphabet)
+    
+    out = []
     if decode == False:
         for i in text:
-            out.append(KEY[alpha.index(i)])
+            out.append(KEY[alphabet.index(i)])
     if decode == True:
         for i in text:
-            out.append(alpha[KEY.index(i)])
+            out.append(alphabet[KEY.index(i)])
     
     return "".join(out)
 
@@ -29,12 +31,27 @@ def atbash(text,key=None,decode=False):
     return substitution(text,"ZYXWVUTSRQPONMLKJIHGFEDCBA")
 
 def substitutionExample():
+    
     print("Example of a Simple Substitution Cipher\n")
     key = "ILOVEZEBRAS"
     print("The key is {}\n".format(key))
     ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
     ctext = substitution(ptext,key)
     dtext = substitution(ctext,key,decode=True)
+    print("Plaintext is:  {}".format(ptext))
+    print("Ciphertext is: {}".format(ctext))
+    print("Decodes As:    {}".format(dtext))
+    
+    
+    print("\n\nExample of A Custom Alphabet")
+    key = "0I1L2O3V4E5Z6E7B8R9AS"
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    print("Alphabet: {}".format(alpha))
+    print("Key: {}\n".format(key))
+    ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
+    ctext = substitution(ptext,key,alphabet=alpha)
+    dtext = substitution(ctext,key,decode=True,alphabet=alpha)
     print("Plaintext is:  {}".format(ptext))
     print("Ciphertext is: {}".format(ctext))
     print("Decodes As:    {}".format(dtext))
@@ -49,5 +66,5 @@ def atbashExample():
     print("Ciphertext is: {}".format(ctext))
     print("Decodes As:    {}".format(dtext))
     
-#substitutionExample()
+substitutionExample()
 #atbashExample()
