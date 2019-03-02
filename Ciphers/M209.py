5b568f52be1b2d8798b06599d1679fafd3afc831
SymmetricChaos <ajfraebel@gmail.com>
2019-03-02 14:05:24 -0500

    Update M209.py
    Removed possibly extraneous information. Will have to compare to a known good simulation

--- Ciphers/M209.py
+++ Ciphers/M209.py
@@ -63,40 +63,14 @@ def keystream(textlen,Lugs,Wheels,Pins,activePins):
         yield K
         for wheel in range(len(Wheels)):
             activePins[wheel] += 1
-
-# Use pins and overlaps to calculate total overlaps
-def totaloverlaps(O,P,i):
-    total = [
-            P[0][i%26]*P[1][i%25]*O['01'],
-            P[0][i%26]*P[2][i%23]*O['02'],
-            P[0][i%26]*P[3][i%21]*O['03'],
-            P[0][i%26]*P[4][i%19]*O['04'],
-            P[0][i%26]*P[5][i%17]*O['05'],
-            P[1][i%25]*P[2][i%23]*O['12'],
-            P[1][i%25]*P[3][i%21]*O['13'],
-            P[1][i%25]*P[4][i%19]*O['14'],
-            P[1][i%25]*P[5][i%17]*O['15'],
-            P[2][i%23]*P[3][i%21]*O['23'],
-            P[2][i%23]*P[4][i%19]*O['24'],
-            P[2][i%23]*P[5][i%17]*O['25'],
-            P[3][i%21]*P[4][i%19]*O['34'],
-            P[3][i%21]*P[5][i%17]*O['35'],
-            P[4][i%19]*P[5][i%17]*O['45']
-            ]
-
-    return sum(total)
     
 
 def M209(text,key,decode=False):
     
     text = ltr2num(text)
     pins = transPins(key[1])
-    LugsByWheel = countLugs(key[2])
     Lugs = lugPos(key[2])
     
-    #   for i in setting:
-    #        print(i)
-    #   print()
     
     Wheels = [
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -106,42 +80,15 @@ def M209(text,key,decode=False):
         "ABCDEFGHIJKLMNOPQRS",
         "ABCDEFGHIJKLMNOPQ"]
     
-
-    
     sh = [15,14,13,12,11,10]
 
-    pins[0] = [1,1,0,1,0,0,0,1,1,0,1,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0]
-    pins[1] = [1,0,0,1,1,0,1,0,0,1,1,1,0,0,1,0,0,1,1,0,1,0,1,0,0]
-    pins[2] = [1,1,0,0,0,0,1,1,0,1,0,1,1,1,0,0,0,1,1,1,1,0,1]
-    pins[3] = [0,0,1,0,1,1,0,1,1,0,0,0,1,1,0,1,0,0,1,1,1]
-    pins[4] = [0,1,0,1,1,1,0,1,1,0,0,0,1,1,0,1,0,0,1]
-    pins[5] = [1,1,0,1,0,0,0,1,0,0,1,0,0,1,1,0,1]
-    
-    
-    LugsByWheel[0] = 2
-    LugsByWheel[1] = 12
-    LugsByWheel[2] = 1
-    LugsByWheel[3] = 5
-    LugsByWheel[4] = 10
-    LugsByWheel[5] = 3
-    
-    Lugs = [[0,0,1,0,0,1], [0,1,0,0,0,0], [0,1,0,0,0,0],
-            [0,0,0,0,0,1], [0,1,0,0,0,0], [0,1,0,0,1,0],
-            [1,0,0,0,0,1], [0,1,0,0,0,0], [0,1,0,0,1,0],
-            [1,0,0,0,1,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
-            [0,0,0,1,1,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
-            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
-            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
-            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
-            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
-            ]
-    
     # For each wheel add up the shift of the wheel and the position of the key
     # letter that is on it.
     activePins = [0,0,0,0,0,0]
     for wheel in range(len(Wheels)):
         activePins[wheel] = sh[wheel] + Wheels[wheel].index(key[0][wheel])
     
+    # Generator that outputs the keystream
     K = keystream(len(text),Lugs,Wheels,pins,activePins)
     
     L = []
@@ -162,8 +109,24 @@ def M209Example():
     for p in [26,25,23,21,19,17]:
          pins.append(random.choices("-+",k=p))
     
-    #for i in pins:
-    #    print("".join(i))
+    pins[0] = [1,1,0,1,0,0,0,1,1,0,1,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0]
+    pins[1] = [1,0,0,1,1,0,1,0,0,1,1,1,0,0,1,0,0,1,1,0,1,0,1,0,0]
+    pins[2] = [1,1,0,0,0,0,1,1,0,1,0,1,1,1,0,0,0,1,1,1,1,0,1]
+    pins[3] = [0,0,1,0,1,1,0,1,1,0,0,0,1,1,0,1,0,0,1,1,1]
+    pins[4] = [0,1,0,1,1,1,0,1,1,0,0,0,1,1,0,1,0,0,1]
+    pins[5] = [1,1,0,1,0,0,0,1,0,0,1,0,0,1,1,0,1]
+    
+        
+    Lugs = [[0,0,1,0,0,1], [0,1,0,0,0,0], [0,1,0,0,0,0], [0,0,0,0,0,1], 
+            [0,1,0,0,0,0], [0,1,0,0,1,0], [1,0,0,0,0,1], [0,1,0,0,0,0], 
+            [0,1,0,0,1,0], [1,0,0,0,1,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,1,0], [0,1,0,0,0,0], [0,0,0,0,1,0], [0,0,0,1,0,0], 
+            [0,1,0,0,0,0], [0,0,0,0,1,0], [0,0,0,1,0,0], [0,1,0,0,0,0], 
+            [0,0,0,0,1,0], [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            [0,0,0,1,0,0], [0,1,0,0,0,0], [0,0,0,0,1,0],
+            ]
+    
+    
     
     # There are 27 pairs of lugs 
     # They can be in one of six effective positions or in one of two ineffective
