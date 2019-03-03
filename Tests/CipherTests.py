72d67d8a2228becbb841b084f323fc5ab36053a6
SymmetricChaos <ajfraebel@gmail.com>
2019-03-03 11:44:33 -0500

    Update CipherTests.py
    Just better organization so I can find things

--- Tests/CipherTests.py
+++ Tests/CipherTests.py
@@ -16,31 +16,41 @@ print()
 from Ciphers.Affine import affine
 from Ciphers.Caesar import caesar
 from Ciphers.Substitution import substitution
+
 from Ciphers.Vigenere import vigenere, multiVigenere, trithemius
 from Ciphers.Beaufort import beaufort, multiBeaufort
 from Ciphers.Autokey import autokey
 from Ciphers.AffineVigenere import affineVigenere
+from Ciphers.Quagmire import quagmire1, quagmire2, quagmire3, quagmire4
+
+from Ciphers.Playfair import playfair
+from Ciphers.TwoSquare import twoSquare
+from Ciphers.FourSquare import fourSquare
+
 from Ciphers.Polybius import polybiusSquare
 from Ciphers.Nihilist import nihilist
 from Ciphers.ADFGVX import ADFGVX
 from Ciphers.Bifid import bifid
 from Ciphers.Trifid import trifid
-from Ciphers.ColumnarTransport import columnarTransport, doubleColumnarTransport
-from Ciphers.Railfence import railfence
+
 from Ciphers.CipherDisk import cipherDisk
-from Ciphers.Chaocipher import chaocipher
+
 from Ciphers.Nomenclator import nomenclator
-from Ciphers.Playfair import playfair
-from Ciphers.FourSquare import fourSquare
+
 from Ciphers.StraddlingCheckerboard import straddlingCheckerboard
-from Ciphers.Enigma import enigma
-from Ciphers.SIGABA import SIGABA
+
 from Ciphers.HillCipher import hillCipher
 from Ciphers.PrimeHillCipher import primeHillCipher
-from Ciphers.TwoSquare import twoSquare
+
+from Ciphers.Chaocipher import chaocipher
 from Ciphers.Hutton import hutton
-from Ciphers.Quagmire import quagmire1, quagmire2, quagmire3, quagmire4
+
 from Ciphers.AMSCO import AMSCO
+from Ciphers.ColumnarTransport import columnarTransport, doubleColumnarTransport
+from Ciphers.Railfence import railfence
+
+from Ciphers.Enigma import enigma
+from Ciphers.SIGABA import SIGABA
 from Ciphers.M209 import M209
 
 # Monoalphabetic
