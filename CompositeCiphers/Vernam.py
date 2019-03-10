0da753dbbaf5136364d7bd596a244e736dc7683f
SymmetricChaos <ajfraebel@gmail.com>
2019-03-10 19:20:16 -0400

    Update Vernam.py

--- CompositeCiphers/Vernam.py
+++ CompositeCiphers/Vernam.py
@@ -1,3 +1,4 @@
+from Ciphers.UtilityFunctions import saveFormat,restoreFormat
 from Codes import morseCode
 from RNG import LFSR
 
@@ -6,15 +7,20 @@ ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
 print(ptext,"\n")
 
 ctext = morseCode(ptext)
-ctext = ctext.replace(" ","")
 ctext = ctext.replace("-","1")
 ctext = ctext.replace(".","0")
 print(ctext,"\n")
+
+ctext, pos, char, case = saveFormat(ctext,"01")
+
 
 bits = LFSR(156,[9,4],10,bits=True)
 
 out = []
 for c,b in zip(ctext,bits):
+    if c == " ":
+        out.append(" ")
+        continue
     if b == 0:
         out.append(c)
     else:
@@ -23,4 +29,5 @@ for c,b in zip(ctext,bits):
         else:
             out.append("0")
 
-print(ctext)
+ftext = restoreFormat("".join(out),pos, char, case)
+print(ftext)
