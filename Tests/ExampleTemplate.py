ac9d31e694ed98c245b4ebd8ac5661f7202101bf
SymmetricChaos <ajfraebel@gmail.com>
2019-03-10 10:45:18 -0400

    Typos

--- Tests/ExampleTemplate.py
+++ Tests/ExampleTemplate.py
@@ -4,8 +4,8 @@ def example(cipher,ptext,key,**kwargs):
     print("{}".format(ptext))
     print("{}".format(ctext))
     if len(ptext) != len(ctext):
-        print("Note that plaintext and cipher text differ in length.")
+        print("Note that plaintext and ciphertext differ in length.")
     if ptext != dtext[:len(ptext)]:
-        print("Decodes Error\n{}\n".format(dtext))
+        print("DECRYPTION ERROR\n{}\n".format(dtext))
     
     print("\n\n")
