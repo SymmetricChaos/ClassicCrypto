16c8382182e802fb52b39f462f4fa701e719e79f
SymmetricChaos <ajfraebel@gmail.com>
2019-03-19 20:54:20 -0400

    Update DisruptedTransposition.py
    Simpler implementation

--- Ciphers/Transposition/DisruptedTransposition.py
+++ Ciphers/Transposition/DisruptedTransposition.py
@@ -1,21 +1,41 @@
 from Ciphers.UtilityFunctions import uniqueRank
 
-key = "BIRTHDAY"
 
-rank = uniqueRank(key)
-print(rank)
-
-S = "THEYHAVEDISCOVEREDTHEQUICKBROWNFOXFLEEATONCE"
-
-
-G = ["" for i in key]
-
-for num in range(len(rank)):
-    L = rank.index(num)+1
+def disruptedTransposition(text,key,decode=False):
     
-    G[num], S = S[:L], S[L:]
+    if len(text) > len(key)**2:
+        raise Exception("Grid will be too small to fit the text")
+    
+    if len(text) < len(key)**2:
+        print("!")
+        nulls = len(key)**2 - len(text)
+        text += "X"*nulls
+    
+    rank = uniqueRank(key)
+    
+    
+    print(rank)
+    print()
 
+    
+    G = ["" for i in key]
+    
+    for num in range(len(rank)):
+        L = rank.index(num)+1
+        G[num], text = text[:L], text[L:]
+    
+    
+    for num in range(len(rank)):
+        rm = len(key) - len(G[num])
+        s,text = text[:rm], text[rm:]
+        G[num] += s.lower()
+        
+    #print(key)
+    for row in G:
+        print(row)
+        
+    
+        
+ptext = "THEYHAVEDISCOVEREDTHATTHEQUICKBROWNFOXJUMPEDOVERTHELAZYDOGFLEEIMMEDIATELY"
 
-print(key)
-for row in G:
-    print(row)
+disruptedTransposition(ptext,"BIRTHDAYS")
