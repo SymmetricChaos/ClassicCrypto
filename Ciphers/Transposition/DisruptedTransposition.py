7f1ca80ad3db152ca2c9b46a2b03eca1860a8c8b
SymmetricChaos <ajfraebel@gmail.com>
2019-03-23 08:44:56 -0400

    Better comments

--- Ciphers/Transposition/DisruptedTransposition.py
+++ Ciphers/Transposition/DisruptedTransposition.py
@@ -4,24 +4,18 @@ from numpy import argsort
 
 def disruptedTransposition(text,key,decode=False,complete=True):
     
-    
     if len(text) > len(key)**2:
         raise Exception("Grid will be too small to fit the text")
     
-
-        
     rank = uniqueRank(key)
     
-    
-
-
     if decode == False:
         
         # Create a blank grid
         G = ["" for i in key]
 
 
-        # Insert nulls if using the completed cipher
+        # Insert nulls if using the completed version cipher
         if complete == True:
             text = addNulls(text,len(key)**2)
         else:
@@ -62,24 +56,28 @@ def disruptedTransposition(text,key,decode=False,complete=True):
         for num in range(len(rank)):
             rm = len(key) - len(G[num])
             s,text1 = text1[:rm], text1[rm:]
-            G[num] += s#.lower()
+            G[num] += s
             
         rowlens = [len(r) for r in G]
         
         # Make a grid of blank spaces
         Gt = [[" " for i in key] for i in key]
+        # Convert the text to a list for easy manipulation
         text = list(text)
-        for x in argsort(rank):
-            for y in range(len(key)):
+        # Read down each column placing a letter only if that position in the
+        # row is valid.
+        for col in argsort(rank):
+            for row in range(len(key)):
 
-                if x < rowlens[y]:
-                    Gt[y][x] = text.pop(0)
+                if col < rowlens[row]:
+                    Gt[row][col] = text.pop(0)
         
+        # Merge the rows of the grid
         X = []
         for i in Gt:
             X.append("".join(i))
 
-
+        # Reach the left and right sides of each row
         out1 = ""
         out2 = ""
         for num in range(len(rank)):
