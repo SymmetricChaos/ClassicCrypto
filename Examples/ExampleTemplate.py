9d155111cfa8b3a9d6462e882a0c7db80fa893d7
SymmetricChaos <ajfraebel@gmail.com>
2019-03-23 08:29:40 -0400

    Removed Printing
    Only prints an error report. Otherwise just returns ctext and dtext to be used.

--- Examples/ExampleTemplate.py
+++ Examples/ExampleTemplate.py
@@ -1,10 +1,7 @@
 def example(cipher,ptext,key,**kwargs):    
     ctext = cipher(ptext,key,False,**kwargs)
     dtext = cipher(ctext,key,True,**kwargs)
-    print("{}".format(ctext))
-    if len(ptext) != len(ctext):
-        print("Note that plaintext and ciphertext differ in length.")
     if ptext != dtext[:len(ptext)]:
         print("DECRYPTION ERROR\n{}\n".format(dtext))
     
-    print("\n")
+    return ctext, dtext
