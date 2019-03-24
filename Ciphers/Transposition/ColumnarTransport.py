38c6f36bd23ff82a77670c90efb7fb695065861e
SymmetricChaos <ajfraebel@gmail.com>
2019-03-23 21:39:27 -0400

    Debug Stuff commented in

--- Ciphers/Transposition/ColumnarTransport.py
+++ Ciphers/Transposition/ColumnarTransport.py
@@ -10,12 +10,12 @@ def columnarTransport(text,key,decode=False,complete=False):
     
     k = uniqueRank(key)
     
-    ## Add nulls if necessary
-    # The nulls are uncommon consonants so that they cannot accidentally form
-    # words at the end of the true message
+    # Determine how many columns
     numcol = len(k)
+    
     numrow,rem = divmod(len(text),numcol)
     longCols = k[:rem]
+    
 
     # If complete is selected nulls are added so that the ciphertext fits
     # perfectly into the grid.
@@ -24,6 +24,7 @@ def columnarTransport(text,key,decode=False,complete=False):
             text = addNulls(text,total_len=numcol*(numrow+1))
         else:
             text = addNulls(text,total_len=numcol*numrow)
+    
     if decode == False:
         
         # Read the text into the rows
@@ -38,24 +39,27 @@ def columnarTransport(text,key,decode=False,complete=False):
         return "".join(out)
     
     if decode == True:
-        
+
         ctr = 0
         L = []
         for i in range(numcol):
             if i in longCols:
                 L.append(text[ctr:ctr+numrow+1])
+                #print("#",text[ctr:ctr+numrow+1],"#")
                 ctr += (numrow+1)
+                
             else:
                 L.append(text[ctr:ctr+numrow])
+                #print("#",text[ctr:ctr+numrow],"#")
                 ctr += numrow
-        
+
         out = []
         
         for row in range(numrow+1):
             for col in k:
                 if len(L[col]) > row:
                     out.append( L[col][row] )
-        
+
         return "".join(out)
     
 ## Double columnar transport is a significant improvement on the single columnar
