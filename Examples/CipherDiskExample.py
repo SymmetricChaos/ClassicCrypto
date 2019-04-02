1454e4e1e53ce2582d447941488edd15d2d64bfa
SymmetricChaos <ajfraebel@gmail.com>
2019-04-02 19:37:14 -0400

    Update CipherDiskExample.py

--- Examples/CipherDiskExample.py
+++ Examples/CipherDiskExample.py
@@ -1,25 +1,33 @@
 from Ciphers import cipherDisk
 from Ciphers.CipherDisk import stepN
+import random
+from ExampleTemplate import example
     
 def cipherDiskExample():
 
     print("Example of a Cipher Disk\n")
+    
+    print("There disk has two rings. The outer ring is stationary while the inner ring can be turned and replaced by the user.")
+    print("\nThe outer ring\nABCDEFGHIJKLMNOPQRSTUVQXYZ0123456789\n")
     
     inner = "1YW7USQ2OM8KIG3ECA9BD4FHJ0LNP5RTVX6Z"
     start = "K"
     inStart = inner[:]
     while inStart[0] != start:
         inStart = stepN(inStart,1)
-    print("Inner Ring Is:\n{}".format(inner))
-    print("Rotated To Start Position {}:\n{}\n".format(start,inStart))
-
+    print("The inner ring\n{}\n".format(inner))
+    
+    print("The starting position for the inner ring is {} so it is rotated until {} is at the top position to get\n{}\n".format(start,start,inStart))
+    
+    random.seed(0)
+    
+    print("Encryption works like a simple substitution cipher but the operator can insert a number at any time then turn the inner disk by that many positions. This allows the cipher to change unpredictably.\n")
+    
     key = [inner,start]
-
     ptext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"
-    ctext = cipherDisk(ptext,key)
-    dtext = cipherDisk(ctext,key,decode=True)
-    print("Plaintext is:\n{}".format(ptext))
-    print("Ciphertext is:\n{}".format(ctext))
-    print("Decodes As:\n{}".format(dtext))
+    
+    ctext, dtext = example(cipherDisk,ptext,key)
+    
+    print("{}\nbecomes\n{}".format(ptext,ctext))
     
 cipherDiskExample()
